a8caa675cfcb601d405a5215bab6790b4cd2bfbd
Spark <piotr.rac@gmail.com>
2022-02-04 16:54:15 +0100

    Added XUnit test project

---
+++ ShoppingListApi.Tests/FixedTaxPolicyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ShoppingListApi.Tests
+{
+    public class FixedTaxPolicyTests
+    {
+        [Fact]
+        public void Apply()
+        {
+
+        }
+    }
+}
