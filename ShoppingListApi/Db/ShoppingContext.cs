7f5d65c201d8a12181d04966fde1ac21938f07a1
Spark <piotr.rac@gmail.com>
2022-02-13 17:24:33 +0100

    Added ShoppingContext.cs in Db folder

---
+++ ShoppingListApi/Db/ShoppingContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingListApi.Db
+{
+    public class ShoppingContext : DbContext
+    {
+        // data persist within DbSet (saves data state of application)
+        public DbSet<ShoppingList> ShoppingLists { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        public ShoppingContext(DbContextOptions<ShoppingContext> options) : base(options)
+        {
+        }
+
+        public ShoppingContext() : base(UseSqlite())
+        {
+        }
+
+        private static DbContextOptions UseSqlite()
+        {
+            return new DbContextOptionsBuilder()
+                .UseSqlite(@"DataSource=ShoppingList.db;")
+                .Options;
+        }
+    }
+}
